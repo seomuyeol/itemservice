97aa2c41cf4c912a33b12f01a6041ec0dceceef8
SEO-my <muyear.seo@gmail.com>
2022-05-18 21:00:35 +0900

    item thyleaf Controller

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import hello.itemservice.domain.item.Item;
@@ -25,6 +26,25 @@ public class BasicItemController {
 		List<Item> items = itemRepository.findAll();
 		model.addAttribute("items", items);
 		return "/basic/items";
+	}
+	
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item",item);
+		return "basic/item";
+	}
+	
+	@GetMapping("/add")
+	public String addForm() {
+		return "basic/addForm";
+	}
+	
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		
+		return "basic/editForm";
 	}
 	
 	/**
